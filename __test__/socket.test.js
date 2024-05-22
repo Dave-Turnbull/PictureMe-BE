e6046fc657b349d21e743f94da6844cdeb9a6649
tatsumakiroll <paul.shoreman@outlook.com>
2024-05-22 14:38:46 +0100

    refactors

--- __test__/socket.test.js
+++ __test__/socket.test.js
@@ -7,66 +7,84 @@ function waitFor(socket, event) {
   });
 }
 
-
-let port;
-
+//start the server
 beforeAll((done) => {
   httpServer.listen(() => {
-    port = httpServer.address().port;
   });
   done()
 });
 
-
+//close the server
 afterAll((done) => {
   io.close();
   done()
 });
 
+//store the connected client sockets to be accessed with clientSockets[n]
+let clientSockets = []
 
-const clientSockets = []
-
-function createClientSocket(done) {
-  const clientSocket = ioc(`http://localhost:${port}`);
+//create a client socket, push it to the client sockets array, then listen for the connection event and resolve when the connection happens
+//returns a promise so can be used with async await or .then()
+function createClientSocket(userID) {
+  const clientSocket = ioc(`http://localhost:${httpServer.address().port}`, {
+    auth: {
+      userID
+    }
+  });
   clientSockets.push(clientSocket)
-  clientSocket.on("connect", done);
+  return new Promise((resolve, reject) => {
+    clientSocket.on("connect", () => {
+      resolve()
+    });
+  })
 }
 
-beforeEach((done) => {
-  createClientSocket(done)
-  createClientSocket(done)
+//create 2 client sockets for every test in the clientSockets array
+beforeEach(async () => {
+  await createClientSocket(1)
+  await createClientSocket()
+  return
 })
+
+//disconenct all client sockets in the clientSocket array after every test
 afterEach((done) => {
   clientSockets.forEach((clientSocket) => {
     if (clientSocket.connected) {
+      //disconnect the client
       clientSocket.disconnect();
+      //remove the client
+      clientSocket.off()
     }
-    done();
   })
+  //reset the clientSockets array so it isnt full of disconnected clients
+  clientSockets = []
+  done();
 })
 
 describe("a headache", () => {
+  it("Clients should connect", () => {
+    clientSockets.forEach((clientSocket) => {
+      expect(clientSocket.connected).toBeTruthy()
+    })
+    expect(clientSockets.length > 0).toBeTruthy()
+  })
   it("should work with an acknowledgement", (done) => {
     clientSockets[0].emit("hi", (arg) => {
       expect(arg).toBe("hola")
       done();
     });
   });
-
   it("should be able to create a user and receive confirmation", async ()=>{
     const username = 'user1'
     const result = await clientSockets[0].emitWithAck('userCreation', username)
     expect(result).toBe(`${username} created`, [username])
   })
-  
   it("should get an array of connected users when joining", async ()=>{
     const username = 'user2'
     await clientSockets[1].emitWithAck('userCreation', username)
     const result = await clientSockets[0].emitWithAck('joinRoom')
     expect(result).toEqual(['user1', 'user2'])
   })
-
-  //userLeft event isn't triggering in the below test, cant work out why
   it("should remove the user from userlist if they leave the game and emit to other users", (done)=>{
     const username = 'user2'
     clientSockets[1].emit('leaveRoom', username)
@@ -74,5 +92,20 @@ describe("a headache", () => {
         expect(message).toBe(`${username} has left the game`)
         done()
     });
+  })
+  it("if user has existing user ID, recieve it from the server", (done) => {
+    clientSockets[0].emit("getUserID", (userID) => {
+      expect(userID).toBe(1)
+      done()
+    })
+  })
+  it("if user has no existing user ID, recieve one from the server", async() => {
+    let receivedUserID = await new Promise((resolve) => {
+      clientSockets[1].emit("getUserID", (userID) => {
+        resolve(userID)
+      })
+    })
+    clientSockets[1].auth.userID = receivedUserID
+    expect(receivedUserID).toBe('a random id')
   })
 });
--- app.js
+++ app.js
@@ -18,7 +18,27 @@ const createUser = (sessionID, username, isHost = false) => {
   users.push({sessionID, username, isHost})
 }
 
+//Create sessions so users can reconnect
+io.use(async (socket, next) => {
+  const userID = socket.handshake.auth.userID;
+  if (userID) {
+    const session = false //change false to room finder function
+    if (session) {
+      //send/trigger room
+      return next()
+    }
+    socket.userID = userID
+    return next()
+  }
+  socket.userID = 'a random id'
+  next()
+})
+
 io.on("connection", (socket) => {
+  socket.on("getUserID", (callbackfunc) => {
+    callbackfunc(socket.userID)
+  })
+
   socket.on("hi", (callbackfunc) => {
     callbackfunc("hola");
   });
@@ -34,6 +54,7 @@ io.on("connection", (socket) => {
   socket.on('leaveRoom', (username) => {
     socket.broadcast.emit('userLeft', `${username} has left the game`)
   })
+
 
   socket.onAny((event, ...args) => {
     console.log('Server triggered event:\n', event, args);
