8553f8ed6a82adea30d45dea6582f4b19a748e40
Jxffxjxke <jaffajake25@gmail.com>
2024-05-22 15:47:06 +0100

    Adds random ID generator

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const http = require("http");
 const { Server } = require("socket.io");
 const { sendLandingPage } = require("./controllers/http-controllers");
+const shortid = require('shortid');
 
 app.use(cors());
 const httpServer = http.createServer(app);
@@ -15,53 +16,54 @@ app.get("/", sendLandingPage);
 let users = [];
 
 const createUser = (sessionID, username, isHost = false) => {
-  users.push({sessionID, username, isHost})
-}
+  users.push({ sessionID, username, isHost });
+};
+const generateID = () => {
+  return shortid.generate();
+};
 
 //Create sessions so users can reconnect
 io.use(async (socket, next) => {
   const userID = socket.handshake.auth.userID;
   if (userID) {
-    const session = false //change false to room finder function
+    const session = false; //change false to room finder function
     if (session) {
       //send/trigger room
-      return next()
+      return next();
     }
-    socket.userID = userID
-    return next()
+    socket.userID = userID;
+    return next();
   }
-  socket.userID = 'a random id'
-  next()
-})
+  //Will need to change to longer hash for user auth.
+  socket.userID = generateID();
+  next();
+});
 
 io.on("connection", (socket) => {
   socket.on("getUserID", (callbackfunc) => {
-    callbackfunc(socket.userID)
-  })
+    callbackfunc(socket.userID);
+  });
 
   socket.on("hi", (callbackfunc) => {
     callbackfunc("hola");
   });
-
-  socket.on('userCreation', (username, response) => {
-
-    users = [...users, username]
-    response(`${username} created`, users)
+  socket.on("userCreation", (username, response) => {
+    users = [...users, username];
+    response(`${username} created`, users);
   });
-  socket.on('joinRoom', (response) => {
-    response(users)
-  })
-  socket.on('leaveRoom', (username) => {
-    socket.broadcast.emit('userLeft', `${username} has left the game`)
-  })
-
+  socket.on("joinRoom", (response) => {
+    response(users);
+  });
+  socket.on("leaveRoom", (username) => {
+    socket.broadcast.emit("userLeft", `${username} has left the game`);
+  });
 
   socket.onAny((event, ...args) => {
-    console.log('Server triggered event:\n', event, args);
+    console.log("Server triggered event:\n", event, args);
   });
   socket.onAnyOutgoing((event, ...args) => {
-    console.log('Server sent an event to client:\n', event, args);
+    console.log("Server sent an event to client:\n", event, args);
   });
 });
 
-module.exports = {app, httpServer, io};
+module.exports = { app, httpServer, io };
